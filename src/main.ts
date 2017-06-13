28c33fddd22766e1b063d525c671779cb049e142
firstor <firstor87@gmail.com>
2017-06-13 09:28:06 -0700

    Fix compile error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
+import {environment} from './env/env';
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {webcomponentsReady} from '@codebakery/origami';
